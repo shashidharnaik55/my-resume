cf831729a7b72bbd175055da5779f4ebbf0703cb
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-07-05 15:55:12 +0530

    Updates

---
+++ precache-manifest.963d2da8d4ff54ec44d20b27e1bce045.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77bcc9cc2423ba1055ae",
+    "url": "/my-resume/static/css/main.6ba85ccc.chunk.css"
+  },
+  {
+    "revision": "77bcc9cc2423ba1055ae",
+    "url": "/my-resume/static/js/main.77bcc9cc.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/my-resume/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "54fe26f0c54799edc228",
+    "url": "/my-resume/static/js/runtime~main.54fe26f0.js"
+  },
+  {
+    "revision": "d7aa7ca83e290d66c4a52a05f0f021a0",
+    "url": "/my-resume/index.html"
+  }
+];
